77bc2dc8a9bef24c92c4fe744693569b79c3a697
prachi <guddip515@gmail.com>
2024-12-21 21:39:37 +0530

    filehandling

---
+++ file_handling/file1.py
@@ -0,0 +1,4 @@
+f=open("D:\\c drive\\cs\Sem6\\Python-harry\\file_handling\\file.txt")
+data=f.read()
+print(data)
+f.close()
---
+++ file_handling/file_write.py
@@ -0,0 +1,4 @@
+st="Hey you are amazing and You are looking happy also"
+f=open("myFile.txt","w")
+f.write(st)
+f.close()
